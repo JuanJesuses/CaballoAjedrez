41c4d9e1f74f421a003caf889e2038584344db34
JuanJesuses <juanj.rodriguez.sanchez@gmail.com>
2019-07-30 22:52:25 +0200

    Se crean los métodos get y set.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -5,4 +5,20 @@ public class Caballo {
 	private Color color;
 	private Posicion posicion;
 	
+	private void setPosicion(Posicion posicion) {
+		this.posicion=posicion;
+	}
+	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	private void setColor(Color color) {
+		this.color=color;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
 }
