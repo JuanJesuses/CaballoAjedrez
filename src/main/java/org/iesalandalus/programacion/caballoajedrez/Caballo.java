d3d1c9f25b4776b89f4bdf8e2c20dc473b291e6f
JuanJesuses <juanj.rodriguez.sanchez@gmail.com>
2019-07-30 23:22:18 +0200

    Se crea el constructor del punto 14. 1b si es blanco, 8b si es negro.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -5,9 +5,27 @@ public class Caballo {
 	private Color color;
 	private Posicion posicion;
 	
-	Caballo (){
+	Caballo(){
 		posicion.setFila(8);
 		posicion.setColumna('b');
+	}
+	
+	Caballo(Color color){
+		
+		this.color=color;
+		
+		if(this.color==Color.BLANCO) {
+			
+			this.posicion.setFila(1);
+			this.posicion.setColumna('b');
+			
+		}else if (this.color==Color.NEGRO) {
+			
+			this.posicion.setFila(8);
+			this.posicion.setColumna('b');
+			
+		}
+		
 	}
 	
 	private void setPosicion(Posicion posicion) {
